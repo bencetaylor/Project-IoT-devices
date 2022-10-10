14a9e34de29a80c63a3fb96c099795961d1296e3
Amrein Milán <amrein7@gmail.com>
2022-10-10 19:31:20 +0200

    DB configuration

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
+
+dotenv.config({ path: './.env' });
+
+// DB connection setup
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+mongoose.connect(DB, {
+  useNewUrlParser: true
+})
+.then(con => {
+  console.log(con.connections);
+  console.log('DB connection established!');
+})
+.catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
+const port = 3000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
