7455e21e56b2fa5a8211aff42ddf7ca4ee5e7cb9
bencetaylor <bencetaylor@gmail.com>
2022-11-16 20:47:33 +0100

    socket + rest api

--- client/client.js
+++ client/client.js
@@ -3,8 +3,7 @@ const app = express()
 const port = 3001
 const http = require("http");
 
-const DEVICE_NAME='X8AKY9XR2G'
-// const DEVICE_ID='5DJIFTT3Q8'
+const DEVICE_NAME='X8AKY9XR2G';
 // const DEVICE_ID='Z4XFLET3TS'
 // const DEVICE_ID='UQKRWSO6OQ'
 
@@ -12,19 +11,26 @@ app.get('/', (req, res) => {
   res.send("I'm a solar sensor")
 })
 
+app.get('/data', (req, res) => {
+  res.json({
+    sunlight: Math.random()*100
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 const data = JSON.stringify({
-  name: 'solar sensor',
-  ip: 'localhost:' + port
+  id: DEVICE_NAME,
+  ip_address: 'localhost:' + port,
+  type: 'solar sensor'
 })
 
 const options = {
   hostname: 'localhost',
   port: 3000,
-  path: '/register',
+  path: '/api/register',
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+const cron = require('node-cron')
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 // function for setting security http headers
 const helmet = require('helmet');
 // function for NoSQL query injection data sanitization
@@ -24,19 +30,78 @@ mongoose.connect(DB, {
   useNewUrlParser: true
 })
 .then(con => {
-  console.log(con.connections);
   console.log('DB connection established!');
 })
 .catch(err => console.log(err));
+
+const _CLIENTS_ONLINE = [
+  // {
+  //   id: 'X8AKY9XR2G',
+  //   ip_address: 'localhost:3001',
+  //   type: 'solar sensor'
+  // }
+];
+
+/**
+ * Cron job to poll the online clients
+ */
+cron.schedule("*/10 * * * * *", function () {
+  console.log('Poll clients:')
+  _CLIENTS_ONLINE.forEach(client => {
+    http.get({ path: '/data', hostname: 'localhost', port: 3001 }, (res) => {
+      let data = ''
+      res.on('data', (chunk) => { data += chunk })
+      res.on('end', () => { console.log(data.sunlight) })
+    })
+  })
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
+// app.get('/send', (req,res) => {
+//   io.emit('msg', 'Hi')
+//   res.send('send')
+// })
+
+/**
+ * REST endpoints
+*/
+app.route('/api/:id')
+.get((req, res) => {
+  console.log(+req.params.id)
+  // TODO call service 
+  const device = _CLIENTS_ONLINE.filter(device => device.id === +req.params.id)[0]
+  res.json(device)
+})
+
+app.route('/api/register')
+.post((req,res)=>{
+  const clientData = req.body
+  console.log(clientData)
+  _CLIENTS_ONLINE.push(clientData)
+  res.json({
+    register: 'success'
+  });
+})
 
 
+/**
+ * socket endpoint to the clients
+*/
+io.on('connection', (socket) => {
+  console.log('socket connected');
+  socket.on('msg', (msg)=>{
+    console.log(msg)
+  })
+
+  socket.on('disconnect', ()=>{
+    console.log('socket disconnected')
+  })
+});
 
 const port = 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 })
